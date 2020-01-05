b2884f32ebfe24009791ffa1209bad755d5ba880
erclu <30255227+erclu@users.noreply.github.com>
2020-01-05 16:33:25 +0100

    fix: use relative file path in subprocess call
    add command to explicitly use chktexrc when found

--- run_action.py
+++ run_action.py
@@ -12,10 +12,19 @@ if not GITHUB_WORKSPACE:
     print("No GITHUB_WORKSPACE environment variable set.")
     sys.exit(1)
 
+os.chdir(GITHUB_WORKSPACE)
+
+
+CHKTEXRC = os.path.abspath(".chktexrc")
+if os.path.exists(CHKTEXRC):
+    print("found local chktexrc")
+    CHKTEX_COMMAND = lambda file: ["chktex", "-q", "-l", CHKTEXRC, file]
+else:
+    CHKTEX_COMMAND = lambda file: ["chktex", "-q", file]
+
 
 def main():
     """main function"""
-    os.chdir(GITHUB_WORKSPACE)
 
     all_files_in_tree = []
     for root, dirs, files in os.walk(".", topdown=True):
@@ -23,43 +32,50 @@ def main():
             if skip_dir in dirs:
                 dirs.remove(skip_dir)
 
-        for filename in files:
-            all_files_in_tree.append(os.path.join(root, filename))
+        for file in files:
+            all_files_in_tree.append(os.path.join(root, file))
 
-    files_to_process = [
-        filename for filename in all_files_in_tree if filename.endswith(".tex")
-    ]
+    files_to_process = [file for file in all_files_in_tree if file.endswith(".tex")]
 
     if not files_to_process:
         print("Found no .tex files to process")
         print("Complete tree found:")
-        for filename in all_files_in_tree:
-            print(filename)
+        for file in all_files_in_tree:
+            print(file)
         sys.exit(0)
 
     files_with_errors = 0
 
-    for filename in files_to_process:
-        print(f"Linting {filename}", end="\n\n")
+    for file in files_to_process:
+        print(f"Linting {file}")
 
-        directory = os.path.dirname(filename)
+        directory = os.path.dirname(file)
+        relative_file = os.path.basename(file)
 
         # run process inside the file's folder
         completed_process = subprocess.run(
-            ["chktex", "-q", filename],
+            CHKTEX_COMMAND(relative_file),
             cwd=directory,
             capture_output=True,
             text=True,
             check=False,
         )
         stdout = completed_process.stdout
+        stderr = completed_process.stderr
 
         if stdout:
             files_with_errors += 1
-            print(stdout)
-        else:
-            print("No warnings found", end="\n\n")
+            print(
+                "----------------------------------------",
+                stdout,
+                "----------------------------------------",
+                sep="\n",
+            )
 
+        if stderr:
+            print("chktex run into errors:", stderr, sep="\n")
+
+    print(f"found {files_with_errors} files with errors")
     sys.exit(files_with_errors)
 
 

4752a2342b9cfc0d89a0200d9f0e50fe54d428d9
Jeremy Kun <j2kun@users.noreply.github.com>
2020-12-13 14:38:31 -0800

    add --inputfiles=0 by default, reorganize for testability

--- run_action.py
+++ run_action.py
@@ -3,58 +3,68 @@
 import os
 import subprocess
 import sys
+from dataclasses import dataclass
+
 
 SKIP_DIRS = set(["venv", ".git", "__pycache__"])
+CHKTEXRC_FILENAME = ".chktexrc"
 
 
-GITHUB_WORKSPACE = os.environ.get("GITHUB_WORKSPACE")
-if not GITHUB_WORKSPACE:
-    print("No GITHUB_WORKSPACE environment variable set.")
-    sys.exit(1)
-
-os.chdir(GITHUB_WORKSPACE)
-
-
-CHKTEXRC = os.path.abspath(".chktexrc")
-if os.path.exists(CHKTEXRC):
-    print("found local chktexrc")
-    CHKTEX_COMMAND = lambda file: ["chktex", "-q", "-l", CHKTEXRC, file]
-else:
-    CHKTEX_COMMAND = lambda file: ["chktex", "-q", file]
-
-
-def main():
-    """main function"""
+def find_files_to_process(github_workspace_path=None, skip_dirs=SKIP_DIRS):
+    '''
+    Return a list of absolute filepaths in the github_workspace_path that
+    end in .tex, i.e., should be linted.
+    '''
+    github_workspace_path = github_workspace_path or os.environ.get(
+        "GITHUB_WORKSPACE")
 
     all_files_in_tree = []
-    for root, dirs, files in os.walk(".", topdown=True):
-        for skip_dir in SKIP_DIRS:
+    for root, dirs, files in os.walk(github_workspace_path, topdown=True):
+        for skip_dir in skip_dirs:
             if skip_dir in dirs:
                 dirs.remove(skip_dir)
 
         for file in files:
             all_files_in_tree.append(os.path.join(root, file))
 
-    files_to_process = [file for file in all_files_in_tree if file.endswith(".tex")]
+    return [file for file in all_files_in_tree if file.endswith(".tex")]
 
-    if not files_to_process:
-        print("Found no .tex files to process")
-        print("Complete tree found:")
-        for file in all_files_in_tree:
-            print(file)
-        sys.exit(0)
 
-    files_with_errors = 0
+def find_chktexrc(github_workspace_path=None, chktexrc_filename=CHKTEXRC_FILENAME):
+    '''
+    Return the absolute path of a chktexrc file in the workspace, if one
+    exists. Otherwise return None.
+    '''
+    os.chdir(github_workspace_path)
+    chktexrc = os.path.abspath(chktexrc_filename)
+    if os.path.exists(chktexrc):
+        return chktexrc
+    else:
+        return None
+
+
+@dataclass
+class Result:
+    filename: str
+    success: bool
+    stdout: str
+    stderr: str
+
+
+def failing_files(files_to_process=None, chktex_command=None):
+    '''
+    Run the given chktex command on the list of files, and return a list of
+    details for the runs that failed.
+    '''
+    error_details = []
 
     for file in files_to_process:
-        print(f"Linting {file}")
-
         directory = os.path.dirname(file)
         relative_file = os.path.basename(file)
 
         # run process inside the file's folder
         completed_process = subprocess.run(
-            CHKTEX_COMMAND(relative_file),
+            chktex_command(relative_file),
             cwd=directory,
             capture_output=True,
             text=True,
@@ -63,21 +73,49 @@ def main():
         stdout = completed_process.stdout
         stderr = completed_process.stderr
 
-        if stdout:
-            files_with_errors += 1
-            print(
-                "----------------------------------------",
-                stdout,
-                "----------------------------------------",
-                sep="\n",
-            )
+        result = Result(
+            filename=file,
+            success=len(stdout) + len(stderr) == 0,
+            stdout=stdout,
+            stderr=stderr
+        )
 
-        if stderr:
-            print("chktex run into errors:", stderr, sep="\n")
+        if not result.success:
+            error_details.append(result)
 
-    print(f"found {files_with_errors} files with errors")
-    sys.exit(files_with_errors)
+    return error_details
 
 
 if __name__ == "__main__":
-    main()
+    GITHUB_WORKSPACE = os.environ.get("GITHUB_WORKSPACE")
+    if not GITHUB_WORKSPACE:
+        print("No GITHUB_WORKSPACE environment variable set.")
+        sys.exit(1)
+
+    files_to_process = find_files_to_process(
+        github_workspace_path=GITHUB_WORKSPACE)
+
+    if not files_to_process:
+        print("Found no .tex files to process")
+        sys.exit(0)
+
+    chktexrc = find_chktexrc(github_workspace_path=GITHUB_WORKSPACE)
+    if chktexrc:
+        print("Found local chktexrc")
+        def chktex_command(file): return ["chktex", "-q", "--inputfiles=0", "-l", chktexrc, file]
+    else:
+        def chktex_command(file): return ["chktex", "-q", "--inputfiles=0", file]
+
+    failing_file_info = failing_files(files_to_process, chktex_command)
+    if failing_file_info:
+        for failing_file_details in failing_file_info:
+           print(
+               "=" * 50,
+               "Linting " + failing_file_details.filename,
+               "-" * 50,
+               failing_file_details.stdout,
+               "=" * 50,
+               sep="\n",
+           )
+
+    sys.exit(len(failing_file_info))
